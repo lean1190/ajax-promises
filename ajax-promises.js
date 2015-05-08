232f0d8efe965314442381a9e6fc9df151290052
Leandro Vilas <leandro.vilas@fluxit.com.ar>
2015-05-08 18:26:50 -0300

    Refactoring for generic AJAX request, prevents duplicated code.

--- ajax-promises.js
+++ ajax-promises.js
@@ -9,8 +9,10 @@ var ajaxp = ajaxp || (function () {
     // Create the object
     var trueajaxp = trueajaxp || {};
 
+    // GET Request
     function _doGet(url) {
-        return new Promise(function (resolve, reject) {
+        return _doAjax(url, {}, 'GET', true);
+        /*new Promise(function (resolve, reject) {
             // Do the usual XHR stuff
             var req = new XMLHttpRequest();
             req.open('GET', url, true);
@@ -35,9 +37,10 @@ var ajaxp = ajaxp || (function () {
 
             // Make the request
             req.send();
-        });
+        });*/
     }
 
+    // GET Request JSON parsed
     function _doGetJSON(url) {
         return _doGet(url).then(JSON.parse).catch(function (err) {
             console.log("getJSON failed for", url, err);
@@ -45,8 +48,10 @@ var ajaxp = ajaxp || (function () {
         });
     }
 
+    // POST Request
     function _doPost(url, params) {
-        return new Promise(function (resolve, reject) {
+        return _doAjax(url, params, 'POST', true);
+        /*new Promise(function (resolve, reject) {
             // Do the usual XHR stuff
             var req = new XMLHttpRequest();
             req.open('POST', url, true);
@@ -76,9 +81,10 @@ var ajaxp = ajaxp || (function () {
 
             // Make the request
             req.send(params);
-        });
+        });*/
     }
 
+    // POST Request JSON parsed
     function _doPostJSON(url, params) {
         return _doPost(url, params).then(JSON.parse).catch(function (err) {
             console.log("postJSON failed for", url, err);
@@ -86,14 +92,31 @@ var ajaxp = ajaxp || (function () {
         });
     }
 
+    // PUT Request
+    function _doPut(url, params) {
+        return _doAjax(url, params, 'PUT', true);
+    }
+
+    // DELETE Request
+    function _doDelete(url, params) {
+        return _doAjax(url, params, 'DELETE', true);
+    }
+
+    // AJAX Generic Request
     function _doAjax(url, params, method, async) {
         return new Promise(function (resolve, reject) {
+
+            // Default value true
+            async = async || true;
+
             // Do the usual XHR stuff
             var req = new XMLHttpRequest();
             req.open(method, url, async);
 
-            //Send the proper header information along with the request
-            req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            if (method.toUpperCase() === "POST") {
+                //Send the proper header information along with the request
+                req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            }
 
             req.onload = function () {
                 // This is called even on 404 etc
@@ -118,6 +141,10 @@ var ajaxp = ajaxp || (function () {
         });
     }
 
+    trueajaxp.ajax = function (url, params, method, async) {
+        return _doAjax(url, params, method, async);
+    };
+
     trueajaxp.get = function (url) {
         return _doGet(url);
     };
@@ -134,8 +161,12 @@ var ajaxp = ajaxp || (function () {
         return _doPostJSON(url, params);
     };
 
-    trueajaxp.ajax = function (url, params, method, async) {
-        return _doAjax(url, params, method, async);
+    trueajaxp.put = function (url, params) {
+        return _doPut(url, params);
+    };
+
+    trueajaxp.delete = function (url, params) {
+        return _doDelete(url, params);
     };
 
     return trueajaxp;
